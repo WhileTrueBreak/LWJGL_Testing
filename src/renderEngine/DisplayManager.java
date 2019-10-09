70ef4677fb349bc511d9b03bdc86eb02093881d7
Jeffrey <jeff.jc2003@gmail.com>
2019-10-09 15:34:41 +1100

    -Added Debug Messages

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -11,19 +11,25 @@ public class DisplayManager {
 
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
-	private static final int FPS_CAP = 60;
+	private static final int FPS_CAP = 120;
 	private static final String TITLE = "Our First Display";
 
 	public static void createDisplay() {
+		System.out.println("Initializing Display...");
 		ContextAttribs attribs = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true);
 		try {
+			System.out.print("\tSetting Display Mode...");
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-			Display.create(new PixelFormat(), attribs);
+			System.out.print("Done!\n\tSetting Title...");
 			Display.setTitle(TITLE);
+			System.out.print("Done!\n\tCreating Display...");
+			Display.create(new PixelFormat(), attribs);
+			System.out.println("Done!");
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
 		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		System.out.println("Done!");
 	}
 
 	public static void updateDisplay() {
@@ -32,7 +38,9 @@ public class DisplayManager {
 	}
 
 	public static void closeDisplay() {
+		System.out.print("Closing Display...");
 		Display.destroy();
+		System.out.println("Done!");
 	}
 
 }
