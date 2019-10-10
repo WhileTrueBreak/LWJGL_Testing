e8e3aab05f7cf377af1245ef1edb93c2143f2127
Jeffrey <jeff.jc2003@gmail.com>
2019-10-10 16:16:52 +1100

    -Changed Window Name

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -12,7 +12,7 @@ public class DisplayManager {
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
 	//private static final int FPS_CAP = 120;
-	private static final String TITLE = "Our First Display";
+	private static final String TITLE = "Display";
 
 	public static long window;
 
