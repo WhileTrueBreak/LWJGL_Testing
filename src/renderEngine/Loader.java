9067f3d1dfc60192cfd4f16bde609b3e08da073b
Jeffrey <jeff.jc2003@gmail.com>
2019-10-13 12:06:25 +1100

    -Added mipmapping

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -40,30 +41,37 @@ public class Loader {
 	public int loadTexture(String path) {
 
 		int textureID;
-		int width, height;
-		ByteBuffer image;
+		int width = 0, height = 0;
+		ByteBuffer texture = ByteBuffer.wrap(new byte[0]);
 
 		try (MemoryStack stack = MemoryStack.stackPush()) {
 			IntBuffer w = stack.mallocInt(1);
 			IntBuffer h = stack.mallocInt(1);
 			IntBuffer comp = stack.mallocInt(1);
-
-			image = STBImage.stbi_load("res/"+path+".png", w, h, comp, 4);
-			if (image == null) {
+			
+			texture = STBImage.stbi_load("res/"+path+".png", w, h, comp, 4);
+			if (texture == null) {
 				System.out.println("Failed to load texture file: "+path+"\n" +
 						STBImage.stbi_failure_reason()
 						);
 			}
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.5f);
 			width = w.get();
 			height = h.get();
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println("Failed to load texture "+path+".png");
+			System.exit(-1);
 		}
-
+		
 		textureID = GL11.glGenTextures();
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
 		textures.add(textureID);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST); //sets MINIFICATION filtering to nearest
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST); //sets MAGNIFICATION filtering to nearest
-		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, texture);
 
 		return textureID;
 	}
